73ba1f8f79678869bfacc939095239b98d040fb7
Cort Gray <cortkatgray@gmail.com>
2022-05-23 20:12:31 -0700

    attempt #3

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,24 +1,28 @@
 const items = document.getElementById(items);
 
 fetch('http://localhost:3000/api/products')
-.then(data => {
-    let products = data.json();
-})
-.then(products => {
-    for (let product of products) {
-        let productItem = document.createElement('a');
-        productItem.innerHTML = '<a href="./product.html?id='+[product.id]+'">';
-        items.appendChild(productItem);
-        let productArticle = document.createElement('article');
-        productItem.appendChild(productArticle);
-        let productImage = document.createElement('img');
-        productImage.innerHTML = '<img src="'+[product.imageURL]+'">';
-        productArticle.appendChild(productImage);
-        let productName = document.createElement('h3');
-        productName.innerHTML = '<h3 class="productName">'+[product.name]+'</h3>';
-        productArticle.appendChild(productName);
-        let productDetails = document.createElement('p');
-        productDetails.innerHTML = '<p class="productDescription">'+[product.description]+'</p>'
-        productArticle.appendChild(productDetails);
-    };
-});
+.then(data => data.json())
+.then(data => createItems(data));
+
+function createItems(array) {
+  const length = array.length;
+  for (let i in array){
+    createItem(array[i]); 
+  }
+}
+
+function createItem(product) {
+  let productItem = document.createElement('a');
+  productItem.innerHTML = '<a href="./product.html?id='+[product.id]+'">';
+  items.appendChild(productItem);
+  let productArticle = document.createElement('article');
+  productItem.appendChild(productArticle);
+  let productImage = document.createElement('img');
+  productArticle.appendChild(productImage);
+  let productName = document.createElement('h3');
+  productName.innerHTML = '<h3 class="productName">'+[product.name]+'</h3>';
+  productArticle.appendChild(productName);
+  let productDetails = document.createElement('p');
+  productDetails.innerHTML = '<p class="productDescription">'+[product.description]+'</p>'
+  productArticle.appendChild(productDetails);
+}
