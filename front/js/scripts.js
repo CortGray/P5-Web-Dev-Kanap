48470ca1baa8d2cfc6c21361662cd2de82414dd5
Cort Gray <cortkatgray@gmail.com>
2022-05-23 19:35:26 -0700

    first attempt at dynamic home page

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,4 +1,24 @@
+const items = document.getElementById(items);
+
 fetch('http://localhost:3000/api/products')
 .then(data => {
     return data.json();
+})
+.then(data => {
+    for (let product of data) {
+        let productItem = document.createElement('a');
+        productItem.innerHTML = '<a href="./product.html?id='+[product.id]+'">';
+        items.appendChild(productItem);
+        let productArticle = document.createElement('article');
+        productItem.appendChild(productArticle);
+        let productImage = document.createElement('img');
+        productImage.innerHTML = '<img src="'+[product.imageURL]+'">';
+        productArticle.appendChild(productImage);
+        let productName = document.createElement('h3');
+        productName.innerHTML = '<h3 class="productName">'+[product.name]+'</h3>';
+        productArticle.appendChild(productName);
+        let productDetails = document.createElement('p');
+        productDetails.innerHTML = '<p class="productDescription">'+[product.description]+'</p>'
+        productArticle.appendChild(productDetails);
+    };
 });
